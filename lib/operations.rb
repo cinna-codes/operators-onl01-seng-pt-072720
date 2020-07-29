d1f989609febece0b6d67e709a6f4781b515dc20
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-07-29 19:00:35 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,8 @@ end
 
 
 def not_safe?(speed)
-# speed > 60 ? return true : return false
-# speed < 40 ? return true : return false
-40 < speed < 60 ? return true : return false
+speed > 60 ? return true : return false
+speed < 40 ? return true : return false
+# 40 < speed < 60 ? return false : return true
 end
 end
