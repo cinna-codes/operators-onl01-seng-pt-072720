814303486cdc1a07959e48ede25eb5a8c21ad58e
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-07-29 19:02:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,18 @@
 def unsafe?(speed)
 if speed > 60
-	return true
+	return "true"
 elsif speed < 40
-	return true
+	return "true"
 else
-	return false
+	return "false"
 end
 end
 
 
 
 def not_safe?(speed)
-speed > 60 ? return true : return false
-speed < 40 ? return true : return false
+speed > 60 ? "true" : "false"
+speed < 40 ? "true" : "false"
 # 40 < speed < 60 ? return false : return true
 end
 end
